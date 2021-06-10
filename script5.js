9fe7893fb8a4355b7c0a37794502255e0472c730
bimbo-ade <bimboadeyemi65@gmail.com>
2021-06-10 19:02:18 +0100

    new

--- script5.js
+++
@@ -1,6 +0,0 @@
-const sideNav = document.querySelector('.side-nav');
-const menu = document.getElementById('menu')
-
-menu.addEventListener('click', function () {
-  sideNav.classList.toggle('navNav');
-})
